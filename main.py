7ca18b6b146cb19c686a124a386114082e14ef7c
Aldrin Jerome Almacin <julienaldrin@gmail.com>
2013-07-17 00:24:14 -0400

    Parallax working

--- main.py
+++ main.py
@@ -5,16 +5,22 @@ from kivy.uix.image import Image
 from kivy.factory import Factory
 
 from parallax_bg import ParallaxBG
+from parallax_bg import ParallaxIMG
 
 import kivy
 
 kivy.require("1.0.9")
 Factory.register("ParallaxBG", ParallaxBG)
+Factory.register("ParallaxIMG", ParallaxIMG)
+
 class DemonSlayer(Widget):
   pass
 
 class DemonSlayerApp(App):
   def build(self):
+    from kivy.base import EventLoop
+    EventLoop.ensure_window()
+    self.window = EventLoop.window
     return DemonSlayer()
 
 
--- parallax_bg.py
+++ parallax_bg.py
@@ -1,4 +1,18 @@
+from kivy.uix.widget import Widget
 from kivy.uix.image import Image
-class ParallaxBG(Image):
+from kivy.clock import Clock
+from kivy.properties import NumericProperty
+
+class ParallaxBG(Widget):
+  pass
+
+class ParallaxIMG(Image):
   def __init__(self, **kwargs):
-    super(ParallaxBG, self).__init__(**kwargs)
+    super(ParallaxIMG, self).__init__(**kwargs)
+    Clock.schedule_interval(self.printer, 0.1)
+
+  def printer(self, dt):
+    if self.x >= -self.width:
+      self.x -= self.speed
+    else:
+      self.x = self.width
