0b7d9fc1ad565cd76ddb039e4072d547af398bbb
Aldrin Jerome Almacin <julienaldrin@gmail.com>
2013-08-02 19:25:06 -0400

    Added weak enemies

--- characters.py
+++ characters.py
@@ -8,6 +8,7 @@ from kivy.clock import Clock
 from kivy.properties import *
 from kivy.app import App
 import constants
+import random
 """
   Class: CharacterManager
   Description: Manages all characters and their behaviour.
@@ -16,12 +17,21 @@ class CharacterManager(Widget):
   """
     Constructor
   """
+  difficulty = NumericProperty(-1)
   def __init__(self, **kwargs):
     super(CharacterManager, self).__init__(**kwargs)
     self.scorer = Scorer()
     self.scorer.pos = constants.SCORER_POS
     self.add_widget(self.scorer)
 
+    self.create_main_character()
+    self.create_weak_enemies()
+
+  """
+    Method: create_main_character
+    Description: Method called to create the avatar to be controlled by the user.
+  """
+  def create_main_character(self):
     mc_sources = {
       constants.STAND_RIGHT: constants.MC_STAND_RIGHT,
       constants.STAND_LEFT: constants.MC_STAND_LEFT,
@@ -36,23 +46,67 @@ class CharacterManager(Widget):
     self.add_widget(self.main_character)
 
   """
+    Method: create_weak_enemies
+    Description: Method called to create the weak enemies.
+  """
+  def create_weak_enemies(self):
+    self.weak_enemies = []
+    for i in range(0, constants.ENEMY_MAX):
+      res = random.randint(0, 2)
+      if res == 0:
+        weak_enemy = WeakEnemy(
+          constants.WC_ROCK,
+          constants.WC_ROCK_IMAGE_DMG,
+          constants.WC_ROCK_DMG * self.difficulty,
+          constants.WC_ROCK_SPEED
+        )
+      elif res == 1:
+        weak_enemy = WeakEnemy(
+          constants.WC_PLAYFULL_GIRL,
+          constants.WC_PLAYFULL_GIRL_IMAGE_DMG,
+          constants.WC_PLAYFULL_GIRL_DMG * self.difficulty,
+          constants.WC_PLAYFULL_GIRL_SPEED,
+          normal_sounds=[constants.WC_PLAYFULL_GIRL_YELL_SOUND_1, constants.WC_PLAYFULL_GIRL_YELL_SOUND_2],
+          die_sounds=constants.WC_PLAYFULL_GIRL_DIE_SOUND,
+          jumper=True
+        )
+      elif res == 2:
+        weak_enemy = WeakEnemy(
+          constants.WC_FROGMAN,
+          constants.WC_FROGMAN_IMAGE_DMG,
+          constants.WC_FROGMAN_DMG * self.difficulty,
+          constants.WC_FROGMAN_SPEED,
+          normal_sounds=[constants.WC_FROGMAN_YELL_SOUND_1, constants.WC_FROGMAN_YELL_SOUND_2],
+          die_sounds=constants.WC_FROGMAN_DIE_SOUND,
+          jumper=True
+        )
+      self.weak_enemies.append(weak_enemy)
+      self.add_widget(weak_enemy)
+
+  """
     Method: on_enter
-    Description: Method to be called when the sprite enters the game.
+    Description: Method to be called when the widget enters the game.
   """
   def on_enter(self):
     self.difficulty = self.parent.parent.difficulty
     Clock.schedule_interval(self.check_collisions, 0)
+    Clock.schedule_interval(self.control_weak_enemies, 0.5)
 
     self.main_character.on_enter()
+    for weak_enemy in self.weak_enemies:
+      weak_enemy.on_enter()
     self.scorer.reset()
 
   """
     Method: on_leave
-    Description: Method to be called when the sprite exits the game.
+    Description: Method to be called when the widget exits the game.
   """
   def on_leave(self):
     Clock.unschedule(self.check_collisions)
+    Clock.unschedule(self.control_weak_enemies)
     self.main_character.on_leave()
+    for weak_enemy in self.weak_enemies:
+      weak_enemy.on_leave()
 
   """
     Method: check_collisions
@@ -60,6 +114,17 @@ class CharacterManager(Widget):
   """
   def check_collisions(self, dt):
     pass
+
+  """
+    Method: control_weak_enemies
+    Description: Method that controls all characters.
+  """
+  def control_weak_enemies(self, dt):
+    res = random.randint(0,1)
+    if res:
+      weak_enemy = random.choice(self.weak_enemies)
+      weak_enemy.run = True
+
 
 #---------------------------------------------------------------------------------
 
@@ -89,7 +154,7 @@ class Character(Image):
 
   """
     Method: on_enter
-    Description: Method to be called when the sprite enters the game.
+    Description: Method to be called when the widget enters the game.
   """
   def on_enter(self):
     self.alive, self.to_right, self.moving, self.jumping, self.attacking  = True, True, True, False, False
@@ -100,7 +165,7 @@ class Character(Image):
 
   """
     Method: on_leave
-    Description: Method to be called when the sprite exits the game.
+    Description: Method to be called when the widget exits the game.
   """
   def on_leave(self):
     Clock.unschedule(self.check_life)
@@ -223,17 +288,29 @@ class MainCharacter(Character):
     self._keyboard.bind(on_key_up=self._on_keyboard_up)
     self.pos = (constants.MC_X, constants.STANDING_Y)
 
+  """
+    Method: keyboard_closed
+    Description: Called when the keyboard is closed.
+  """
   def _keyboard_closed(self):
     if self.alive and self.keyboard_on:
       self._keyboard.unbind(on_key_down=self._on_keyboard_down)
       self._keyboard.unbind(on_key_up=self._on_keyboard_up)
       self._keyboard = None
 
+  """
+    Method: on_touch_down
+    Description: Called when the mouse is clicked.
+  """
   def on_touch_down(self, touch):
     if self.alive:
       self.attack()
       return super(Character, self).on_touch_down(touch)
 
+  """
+    Method: on_keyboard_down
+    Description: Called when a key is pressed.
+  """
   def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
     if self.alive and self.keyboard_on:
       if self.on_battle:
@@ -245,6 +322,10 @@ class MainCharacter(Character):
         if keycode[1] == "w":
           self.jump()
 
+  """
+    Method: on_keyboard_up
+    Description: Called when a key is up.
+  """
   def _on_keyboard_up(self, keyboard, keycode):
     if self.alive and self.keyboard_on and self.on_battle:
       if keycode[1] == "d":
@@ -255,7 +336,7 @@ class MainCharacter(Character):
   # OVERRIDEN METHODS
   """
     Method: on_enter
-    Description: Method to be called when the sprite enters the game.
+    Description: Method to be called when the widget enters the game.
   """
   def on_enter(self):
     super(MainCharacter, self).on_enter()
@@ -264,7 +345,7 @@ class MainCharacter(Character):
 
   """
     Method: on_leave
-    Description: Method to be called when the sprite exits the game.
+    Description: Method to be called when the widget exits the game.
   """
   def on_leave(self):
     super(MainCharacter, self).on_leave()
@@ -361,4 +442,117 @@ class Scorer(Label):
   def reset(self):
     self.score = 0
 
+#---------------------------------------------------------------------------------
+"""
+  Class: WeakEnemy
+  Description: The weak enemy that attacks the user. Dies in one hit.
+"""
+class WeakEnemy(Image):
+  """
+    Constructor
+  """
+  def __init__(self, source, damaged_img, dmg, speed, normal_sounds=None, die_sounds=None, jumper=False, **kwargs):
+    super(WeakEnemy, self).__init__(source=constants.WC_ROCK, x=constants.CHARACTER_STORAGE, y=constants.STANDING_Y)
+    self.the_source = source
+    self.dmg = dmg
+    self.speed = speed
+    self.jumper = jumper
+    self.damaged_img = damaged_img
+    self.normal_sounds = normal_sounds
+    self.die_sounds = die_sounds
+    self.pos = (constants.CHARACTER_STORAGE, constants.STANDING_Y)
+    self.run = False
+    self.jumping = False
+
+  """
+    Method: damaged
+    Description: Method that shows the weak enemy damaged.
+  """
+  def damaged(self):
+    self.source = self.damaged_img
+    self.size = self.texture_size
+    anim = Animation(x=self.x + 200, duration=0.3)
+    anim.start(self)
+    Clock.schedule_once(self.reset, 0.3)
+
+  """
+    Method: start_running
+    Description: Move the weak enemy closer to the main character
+  """
+  def start_running(self, dt):
+    if self.run:
+      self.x -= self.speed
+      res = random.randint(0,10)
+      if not self.jumping and res == 0:
+        self.jump()
+
+  """
+    Method: jump
+    Description: Method used to make the weak enemy jump.
+  """
+  def jump(self):
+    if not self.jumping and self.jumper:
+      anim = Animation(
+        y=constants.JUMP_HEIGHT,
+        duration=constants.JUMP_DURATION
+      ) + Animation(
+        y=constants.STANDING_Y,
+        duration=constants.JUMP_DURATION
+      )
+      anim.start(self)
+
+  """
+    method: check_jumping
+    description: method that checks if the character is jumping.
+  """
+  def check_jumping(self, dt):
+    if self.y != constants.STANDING_Y:
+      self.jumping = True
+    else:
+      self.jumping = False
+
+  """
+    Method: normal_sound
+    Description: Play the normal sound.
+  """
+  def normal_sound(self):
+    if self.normal_sounds != None:
+      SoundLoader.load(random.choice(self.normal_sounds)).play()
+
+  """
+    Method: die_sound
+    Description: Play the die sound.
+  """
+  def die_sound(self):
+    if self.die_sounds != None:
+      SoundLoader.load(random.choice(self.die_sounds)).play()
+
+  """
+    Method: on_enter
+    Description: Method to be called when the widget enters the game.
+  """
+  def on_enter(self):
+    self.pos = (constants.CHARACTER_STORAGE, constants.STANDING_Y)
+    self.source = self.the_source
+    self.size = self.texture_size
+    self.run = False
+    Clock.schedule_interval(self.start_running, 0)
+    Clock.schedule_interval(self.check_jumping, 0)
+
+  """
+    Method: on_leave
+    Description: Method to be called when the widget exits the game.
+  """
+  def on_leave(self):
+    self.reset()
+    Clock.unschedule(self.start_running)
+    Clock.unschedule(self.check_jumping)
+
+  """
+    Method: reset
+    Description: Sends the weak enemy back to its storage.
+  """
+  def reset(self):
+    self.pos = (constants.CHARACTER_STORAGE, constants.STANDING_Y)
+    self.run = False
 #---------------------------------------------------------------------------------
